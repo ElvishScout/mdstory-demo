e70643e09a38e15c66733beead5bab4c3b538920
Jiakai Jiang <elvishscoutustc@outlook.com>
2026-06-20 10:52:52 +0800

    更新模板

---
+++ template/src/utils.ts
@@ -0,0 +1,17 @@
+export function escapeHtml(text: string) {
+  return text
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
+export function unescapeHtml(text: string) {
+  return text
+    .replace(/&amp;/g, "&")
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&quot;/g, '"')
+    .replace(/&apos;/g, "'");
+}
