891685d689d5acead69fdd4f1029d676445c6121
Jiakai Jiang <elvishscoutustc@outlook.com>
2026-07-23 09:40:09 +0800

    移除无用 vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,74 +1,8 @@
 import path from "node:path";
-import fs from "fs/promises";
 
-import { defineConfig, PluginOption } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
-const fileExists = async (filename: string) => {
-  try {
-    await fs.stat(filename);
-    return true;
-  } catch {
-    return false;
-  }
-};
-
-const pluginStrictRoute = (): PluginOption => {
-  return {
-    name: "vite-strict-route",
-    configureServer(server) {
-      const { root, publicDir } = server.config;
-
-      server.middlewares.use(async (req, res, next) => {
-        const { pathname } = new URL(req.url ?? "/", `http://${req.headers.host}`);
-        const relativePath = path.relative("/", pathname);
-
-        if (
-          relativePath.startsWith("@") ||
-          (await fileExists(path.resolve(root, relativePath))) ||
-          (await fileExists(path.resolve(publicDir, relativePath)))
-        ) {
-          next();
-          return;
-        }
-
-        if (relativePath === "template.html") {
-          try {
-            const template = await fs.readFile(path.resolve(__dirname, "dist/template.html"));
-            res.writeHead(200, { "content-type": "text/html" });
-            res.end(template);
-            return;
-          } catch {
-            /* empty */
-          }
-        }
-
-        res.writeHead(404, "file not found");
-        res.end();
-      });
-    },
-  };
-};
-
-const pluginFixBrotliWasm = (): PluginOption => {
-  return {
-    name: "vite-plugin-fix-brotli-wasm",
-    configureServer(server) {
-      server.middlewares.use(async (req, res, next) => {
-        if (req.url?.endsWith("brotli_wasm_bg.wasm")) {
-          const filePath = path.resolve(__dirname, "node_modules/brotli-wasm/pkg.web/brotli_wasm_bg.wasm");
-          const fileContent = await fs.readFile(filePath);
-          res.setHeader("Content-Type", "application/wasm");
-          res.statusCode = 200;
-          res.end(fileContent);
-        } else {
-          next();
-        }
-      });
-    },
-  };
-};
 
 const root = path.resolve(__dirname, "pages");
 const envDir = path.resolve(__dirname);
@@ -80,7 +14,7 @@ export default defineConfig({
       "@": path.resolve(__dirname),
     },
   },
-  plugins: [react(), tailwindcss(), pluginStrictRoute(), pluginFixBrotliWasm()],
+  plugins: [react(), tailwindcss()],
   root,
   envDir,
   publicDir,
